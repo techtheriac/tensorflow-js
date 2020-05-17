be99f8f86c8716b533f6e8481078553afc5c23fb
techtheriac <mavyjayann@gmail.com>
2020-05-17 12:12:16 +0100

    First Tensor with a defined shape created

--- src/main.js
+++ src/main.js
@@ -1,9 +1,7 @@
 import * as tf from "@tensorflow/tfjs";
-
+// Defines shape (dimension of Tensor)
 const shape = [4, 2];
-
+//Tensor (Immutable)
 const data = tf.tensor([3, 4, 6, 7, 6, 7, 7, 9], shape);
-
+//Print instance for data
 data.print();
-
-console.log("Testing");
