08e937891851c71deec719846d5c4aaec30fb87c
techtheriac <mavyjayann@gmail.com>
2020-05-17 12:24:17 +0100

    Tensor: data2, Mutated

--- src/main.js
+++ src/main.js
@@ -3,5 +3,13 @@ import * as tf from "@tensorflow/tfjs";
 const shape = [4, 2];
 //Tensor (Immutable)
 const data = tf.tensor([3, 4, 6, 7, 6, 7, 7, 9], shape);
+
+//Set Variable (Mutable) with Zeros method
+const data2 = tf.variable(tf.zeros([8]));
+
 //Print instance for data
-data.print();
+data2.print();
+
+//Mutating data2 with data of  one dimension
+data2.assign(tf.tensor1d([1, 0, 4, 6, 89, 9, 0, 9]));
+data2.print();
